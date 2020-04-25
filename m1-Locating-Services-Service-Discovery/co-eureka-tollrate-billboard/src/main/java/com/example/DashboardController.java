f52513f13d3b666474f361b8395b3bcb8f831255
Prateek <prateek.ashtikar512@gmail.com>
2020-04-25 22:48:12 +0530

    Locating Services at Runtime Using Service Discovery

---
+++ m1-Locating-Services-Service-Discovery/co-eureka-tollrate-billboard/src/main/java/com/example/DashboardController.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class DashboardController {
+	
+	@LoadBalanced
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+	
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@RequestMapping("/dashboard")
+	public String GetTollRate(@RequestParam int stationId, Model m) {
+		
+		TollRate tr = restTemplate.getForObject("http://toll-service/tollrate/" + stationId, TollRate.class);
+		System.out.println("TOLL_RATE = "+ tr.toString());
+		
+		System.out.println("stationId: " + stationId);
+		m.addAttribute("rate", tr.getCurrentRate());
+		return "dashboard";
+	}
+}
---
+++ m1-Locating-Services-Service-Discovery/co-eureka-tollrate-service/src/main/java/com/example/TollRateController.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TollRateController {
+
+	@GetMapping(value = "/tollrate/{stationId}")
+	public TollRate GetTollRate(@PathVariable int stationId) {
+		System.out.println("Station requested: " + stationId);
+
+		TollRate tr;
+		switch (stationId) {
+		case 1:
+			tr = new TollRate(stationId, new BigDecimal("0.55"), Instant.now().toString());
+			break;
+		case 2:
+			tr = new TollRate(stationId, new BigDecimal("1.05"), Instant.now().toString());
+			break;
+		case 3:
+			tr = new TollRate(stationId, new BigDecimal("0.60"), Instant.now().toString());
+			break;
+		default:
+			tr = new TollRate(stationId, new BigDecimal("1.00"), Instant.now().toString());
+			break;
+		}
+
+		return tr;
+	}
+}
